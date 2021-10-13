b4950e3156bd584a9a6d7004f64f247667ecef5c
“seriouszyx” <674965440@qq.com>
2021-10-13 15:28:19 +0800

    add config.py

---
+++ cat_and_dog/config.py
@@ -0,0 +1,6 @@
+DATA_PATH = 'data'
+TRAIN_PATH = f'{DATA_PATH}/train/'
+VAL_PATH = f'{DATA_PATH}/val/'
+TRAIN_RATE = 0.8
+BATCH_SIZE = 64
+NUM_EPOCHS = 20
--- cat_and_dog/main.py
+++ cat_and_dog/main.py
@@ -2,20 +2,21 @@ import torch
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 
+from cat_and_dog import config
 from cat_and_dog.dataset import MyData, dataset_split
 from cat_and_dog.model import *
 from cat_and_dog.train import train, evaluate
 
 if __name__ == '__main__':
     # Prepare the data
-    DATA_PATH = 'data'
-    train_path = f'{DATA_PATH}/train/'
-    val_path = f'{DATA_PATH}/val/'
+    TRAIN_PATH = config.TRAIN_PATH
+    VAL_PATH = config.VAL_PATH
+    BATCH_SIZE = config.BATCH_SIZE
 
-    train_ds, test_ds = dataset_split(MyData(train_path), 0.8)
-    train_loader = DataLoader(train_ds, batch_size=64, shuffle=True)
-    test_loader = DataLoader(test_ds, batch_size=64, shuffle=True)
-    val_loader = DataLoader(MyData(val_path), batch_size=64, shuffle=True)
+    train_ds, test_ds = dataset_split(MyData(TRAIN_PATH), config.TRAIN_RATE)
+    train_loader = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True)
+    test_loader = DataLoader(test_ds, batch_size=BATCH_SIZE, shuffle=True)
+    val_loader = DataLoader(MyData(VAL_PATH), batch_size=BATCH_SIZE, shuffle=True)
 
     # Build the model
     model = my_resnet_50(pretrained=True)
@@ -24,7 +25,7 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     optimizer = Adam(model.parameters(), lr=1e-4)
     criterion = torch.nn.CrossEntropyLoss()
-    for epoch in range(20):
+    for epoch in range(config.NUM_EPOCHS):
         train(model, epoch, train_loader, device, optimizer, criterion)
         evaluate(model, val_loader, device, criterion, mode='val')
 
